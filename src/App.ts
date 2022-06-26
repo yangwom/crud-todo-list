64fc5fa97633ea962769bf65409aed98fc19f798
yang vieira <yangflix3@gmail.com>
2022-06-25 21:30:35 -0400

    eslint

--- src/App.ts
+++ src/App.ts
@@ -2,22 +2,22 @@ import express from 'express';
 import cors from 'cors';
 
 class App {
-  public express: express.Application;
+	public express: express.Application;
 
-  constructor() {
-    this.express = express();
-    this.routes();
-    this.middlewares();
-  }
+	constructor() {
+		this.express = express();
+		this.routes();
+		this.middlewares();
+	}
 
-  private middlewares(): void {
-    this.express.use(cors());
-    this.express.use(express.json());
-  }
+	private middlewares(): void {
+		this.express.use(cors());
+		this.express.use(express.json());
+	}
 
-  private routes(): void {
-    this.express.get('/', (req, res) => res.send('hello yang '));
-  }
+	private routes(): void {
+		this.express.get('/', (req, res) => res.send('hello yang '));
+	}
 }
 
 export default new App().express;
