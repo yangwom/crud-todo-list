936b8c0e313f226f7b72267d3b4431f32241ca85
yang vieira <yangflix3@gmail.com>
2022-07-03 13:28:57 -0400

    user getAll associate mode task

--- backend/src/service/UserService.ts
+++ backend/src/service/UserService.ts
@@ -1,11 +1,13 @@
 import { Model } from 'sequelize/types';
+import tasksModel from '../model/tasksModel';
 import model from '../model/usersModel';
 
 class UserService {
 	data: Model[];
 	async getAll() {
 		this.data = await model.findAll({
-			attributes: { exclude: ['password']}
+			include: [{ model: tasksModel }],
+			attributes: { exclude: ['password'] },
 		});
 
 		return this.data;
