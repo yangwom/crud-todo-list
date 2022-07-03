9d38a9953b52a1af1706b056b8499815b2bee91a
yang vieira <yangflix3@gmail.com>
2022-07-03 15:44:51 -0400

    service

--- backend/src/service/UserService.ts
+++ backend/src/service/UserService.ts
@@ -1,12 +1,10 @@
 import { Model } from 'sequelize/types';
-import tasksModel from '../model/tasksModel';
 import model from '../model/usersModel';
 
 class UserService {
 	data: Model[];
 	async getAll() {
 		this.data = await model.findAll({
-			include: [{ model: tasksModel }],
 			attributes: { exclude: ['password'] },
 		});
 
