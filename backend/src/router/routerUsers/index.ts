8479808cd8b3ed1ffd75ed06771b406b75013131
yang vieira <yangflix3@gmail.com>
2022-07-05 10:02:07 -0400

    routerUsers

--- backend/src/router/routerUsers/index.ts
+++ backend/src/router/routerUsers/index.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import UserController from '../../controller/UserController';
 import userValidate from '../../middleware/userValidate';
+import authMiddleware from '../../middleware/validateToken';
 
 
 const routerUsers = Router();
 
-routerUsers.get('/', new UserController().getAll);
-routerUsers.get('/:id', new UserController().getById);
-routerUsers.post('/', userValidate, new UserController().create);
+routerUsers.get('/', authMiddleware, new UserController().getAll);
+routerUsers.get('/:id',authMiddleware, new UserController().getById);
+routerUsers.post('/', authMiddleware, userValidate, new UserController().create);
+routerUsers.delete('/:id',authMiddleware, new UserController().delete);
 
 export default routerUsers;
