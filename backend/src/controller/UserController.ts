d45652b9bf7792cf451889dee0142b72ea6ddf9c
yang vieira <yangflix3@gmail.com>
2022-07-05 10:00:22 -0400

    UserController

--- backend/src/controller/UserController.ts
+++ backend/src/controller/UserController.ts
@@ -1,6 +1,7 @@
 import UserService from '../service/UserService';
 import { Response, Request, NextFunction } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import authorizedUser from '../interface/reqUser';
 class UserController {
 	async getAll(req: Request, res: Response, next: NextFunction) {
 		try {
@@ -22,8 +23,10 @@ class UserController {
 		}
 	}
 
-	async getById(req: Request, res: Response, next: NextFunction) {
+	async getById(req: authorizedUser, res: Response, next: NextFunction) {
 		const { id } = req.params;
+		const { user } = req;
+		console.log(user);
 		try {
 			const data = await new UserService().getById(id);
 
@@ -32,6 +35,19 @@ class UserController {
 			next(error);
 		}
 	}
+
+	async delete(req: Request, res: Response, next: NextFunction) {
+		const { id } = req.params;
+		try {
+			await new UserService().delete(id);
+
+			return res.status(StatusCodes.NO_CONTENT).json();
+		} catch (error) {
+			next(error);
+		}
+	}
+
+
 }
 
 export default UserController;
